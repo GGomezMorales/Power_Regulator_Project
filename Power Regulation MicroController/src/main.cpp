51b63e752506d30e7af1e9d822b52943b9fc5931
DanD1511 <ddacunha012001@gmail.com>
2023-11-10 13:38:32 -0500

    WebSocket Implemantation

--- Power Regulation MicroController/src/main.cpp
+++ Power Regulation MicroController/src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebSocketsServer.h>
 
-// Pinout ESP32 Wemos Lolin32: GPIO 16, 34, 35
+const char *ssid = "rwork";
+const char *password = "MoniP3P3";
+
+const int webSocketPort = 81;
+WebSocketsServer webSocket = WebSocketsServer(webSocketPort);
+
 const byte gateTriacPin = 16;
 const byte zeroCrossingPin = 34;
 
-// Variables
-unsigned long delayTime = 0; // Delay time in microseconds between the zero crossing and the gate signal.
+int angle = 90;
+unsigned long delayTime = 0;
 volatile bool zeroCrossed = false;
 
 void handleZeroCrossing()
@@ -13,24 +20,56 @@ void handleZeroCrossing()
   zeroCrossed = true;
 }
 
-void setup()
-{
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Desconectado!\n", num);
+      break;
+    case WStype_CONNECTED: {
+      IPAddress ip = webSocket.remoteIP(num);
+      Serial.printf("[%u] Conectado desde %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+    }
+    break;
+    case WStype_TEXT:
+      Serial.printf("[%u] Recibido: %s\n", num, payload);
+      angle = atoi((char *)payload); 
+      angle = constrain(angle, 0, 180);
+      break;
+  }
+}
+
+void setup() {
   Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando a la red WiFi...");
+  }
+
+  // Muestra la dirección IP de la ESP32 en el monitor serie
+  Serial.print("Dirección IP de la ESP32: ");
+  Serial.println(WiFi.localIP());
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
   pinMode(gateTriacPin, OUTPUT);
   pinMode(zeroCrossingPin, INPUT_PULLDOWN);
-  attachInterrupt(digitalPinToInterrupt(zeroCrossingPin), handleZeroCrossing, FALLING);
+  attachInterrupt(digitalPinToInterrupt(zeroCrossingPin), handleZeroCrossing, RISING);
 
+  Serial.println("Servidor WebSocket iniciado");
 }
-  
-void loop()
-{
-  // angle = 90;
 
-  if (zeroCrossed)
-  {
-    digitalWrite(gateTriacPin, HIGH);
+void loop() {
+  if (zeroCrossed) {
     delayMicroseconds(50);
+    delayTime = map(angle, 0, 180, 8333, 0);
+    delayMicroseconds(delayTime);
+    digitalWrite(gateTriacPin, HIGH);
+    delayMicroseconds(100); // Duración del pulso de encendido (ajústalo según tus necesidades).
     digitalWrite(gateTriacPin, LOW);
     zeroCrossed = false;
   }
+
+  webSocket.loop();
 }
